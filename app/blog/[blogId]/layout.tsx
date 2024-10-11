4f4be7391836dd1d8484575c663588e070557b98
Gevaun <geniepinguin@gmail.com>
2024-10-11 18:51:47 -0500

    Refactor blog layout and page components

---
+++ app/blog/[blogId]/layout.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { MoveLeftIcon } from "lucide-react";
+
+export default function Layout({ children }) {
+  const router = useRouter();
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={router.back}
+          className="flex items-center gap-2 text-sm opacity-80 hover:opacity-100"
+        >
+          <MoveLeftIcon className="w-4"/>
+          Back to blogs
+        </button>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+}
--- app/blog/[blogId]/page.tsx
+++ app/blog/[blogId]/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import { useRouter } from "next/navigation";  
 import { motion } from "framer-motion";
+import { MoveLeftIcon } from "lucide-react";
 
 export default function Blog({ params }: { params: { blogId: string } }) {
+  const router = useRouter();  // get router object
+
   return (
+    <div>
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
       <div>
-        <h1 className="font-bold text-lg tracking-tight">Blog</h1>
-        {params.blogId}
+        <h1 className="font-bold text-lg tracking-tight">Blog {params.blogId}</h1>
       </div>
     </motion.div>
+
+    </div>
   );
 }
