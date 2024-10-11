c1a30174bcc41262eb703557f2971d8715f9c9a4
Gevaun <geniepinguin@gmail.com>
2024-10-11 09:28:44 -0500

    Refactor page components and remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import Image from "next/image";
 import BlogElements from "@/components/app/blogElements";
 import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-    >
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
       <div>
         <h1 className="font-bold text-lg tracking-tight">Featured Posts</h1>
         <div className="mt-4 space-y-4">
--- components/app/blogElements.tsx
+++ components/app/blogElements.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
-import { StickyNoteIcon, MoveUpRightIcon } from "lucide-react";
+import { MoveUpRightIcon } from "lucide-react";
 import { motion } from "framer-motion";
 
 const blogPosts = [
