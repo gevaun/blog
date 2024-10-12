e3bc1bb0404c03573c5d6c37065cfe5c1dcf0728
Gevaun <geniepinguin@gmail.com>
2024-10-12 15:09:33 -0500

    Refactor page component to improve animation and layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,21 @@
 
 import BlogElements from "@/components/app/blogs";
 import { motion } from "framer-motion";
+import { div } from "framer-motion/client";
 
 export default function Home() {
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-      <div>
+    <div>
+      <div className="mb-4">
         <h1 className="font-bold text-lg tracking-tight">Featured Posts</h1>
-        <div className="mt-4 space-y-4">
-          <BlogElements />
-        </div>
       </div>
-    </motion.div>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+        <div>
+          <div className="mt-4 space-y-4">
+            <BlogElements />
+          </div>
+        </div>
+      </motion.div>
+    </div>
   );
 }
