1ee26fe9482d315dc7616920bf3502ca2326322a
Gevaun <geniepinguin@gmail.com>
2024-10-12 12:10:26 -0500

    Add ThemeProvider component for managing themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navigation from "@/components/navigation";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -29,8 +31,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navigation />
-        <div className="mt-20 mx-auto container">{children}</div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navigation />
+          <div className="mt-20 mx-auto container">{children}</div>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,22 +1,74 @@
-'use client'
+"use client";
 
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { SunIcon, MoonStarIcon, MonitorIcon } from "lucide-react";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export default function navigation() {
-    const styles = {
-        nav: "flex justify-between items-center p-4",
-        link: "text-xl font-bold tracking-tighter",
-    }
+  const { setTheme } = useTheme();
 
-    return (
-        <nav className="flex justify-between items-center p-4">
-            <div className="flex items-center gap-4">
-                <Link href="/" className="text-xl font-bold tracking-tighter">Blog.</Link>
-            </div>
-            <div className="flex items-center gap-4">
-                <Link href="/about">About</Link>
-            </div>
-        </nav>
-    )
+  const themeIconStyle =
+    "w-4 h-4 duration-400 ease-in-out transition dark:hover:stroke-white";
+ 
+  //   const themeIcon = theme ? (
+  //   <SunIcon
+  //     className={themeIconStyle}
+  //     onClick={() => {
+  //       setTheme("light")
+  //       setThemeState("light");
+  //     }}
+  //   />
+  // ) : (
+  //   <MoonStarIcon
+  //     className={`${themeIconStyle}`}
+  //     onClick={() => {
+  //       setTheme("dark")
+  //       setThemeState("dark");
+  //     }}
+  //   />
+  // );
+
+  return (
+    <nav className="flex justify-between items-center p-4">
+      <div className="flex items-center gap-4">
+        <Link href="/" className="text-xl font-bold tracking-tighter">
+          Blog.
+        </Link>
+      </div>
+      <div className="flex items-center gap-4">
+        <Link href="/about">About</Link>
+          <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonStarIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+      </div>
+    </nav>
+  );
 }
