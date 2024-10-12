793b22fa9f31ed6da6a583e3853bad42e00fe6c3
Gevaun <geniepinguin@gmail.com>
2024-10-11 19:28:57 -0500

    Refactor layout.tsx to use TypeScript type for children prop

--- app/blog/[blogId]/layout.tsx
+++ app/blog/[blogId]/layout.tsx
@@ -3,7 +3,9 @@
 import { useRouter } from "next/navigation";
 import { MoveLeftIcon } from "lucide-react";
 
-export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   const router = useRouter();
 
   return (
@@ -13,7 +15,7 @@ export default function Layout({ children }: Readonly<{ children: React.ReactNod
           onClick={router.back}
           className="flex items-center gap-2 text-sm opacity-80 hover:opacity-100"
         >
-          <MoveLeftIcon className="w-4"/>
+          <MoveLeftIcon className="w-4" />
           Back to blogs
         </button>
       </div>
