73ccb7583e8742b03e790ae91854915c4ba8e0d2
Gevaun <geniepinguin@gmail.com>
2024-11-25 22:30:37 -0500

    Declare prop types

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { CustomMDX } from "@/app/components/mdx";
 import { getBlogPosts } from "@/app/utils/utils";
@@ -10,7 +11,17 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function Blog({ params }) {
+type BlogProps = {
+  params: {
+    slug: string;
+    title: string;
+    description: string;
+    content: string;
+  };
+};
+
+export default async function Blog(props: BlogProps) {
+  const params = await props.params;
   let post = getBlogPosts().find((post) => post.slug === params.slug);
 
   if (!post) {
@@ -31,6 +42,9 @@ export default function Blog({ params }) {
           }),
         }}
       />
+    <a href="/" className="text-blue-500 hover:underline">
+        &larr; Back to Blogs
+    </a>
       <h1 className="title font-semibold text-2xl tracking-tighter">
         {post.title}
       </h1>
